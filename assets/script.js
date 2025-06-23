74f07b374845151d24359865fa07f31aed22b420
Matt <50344355+mkrell23@users.noreply.github.com>
2025-06-23 10:10:49 -0400

    Board marking and turn taking

--- assets/script.js
+++ assets/script.js
@@ -22,21 +22,56 @@ const board = (function(){
     function markSquare(marker, position){
         if ( playBoard[position] === ""){
             playBoard[position] = marker;
+            return playBoard;
         }
-        return playBoard
+        else {
+            return false;
+        }
     }
 
     return {showBoard, markSquare, newBoard};
 })()
 
 const game = (function(){
-    function playRound(){
+    const player1 = newPlayer("X")
+    const player2 = newPlayer("O")
 
+    let player1Turn = true;
+
+    function newGame(){
+        player1Turn = true;
+        board.newBoard();
     }
+
+    function playRound(square){
+        currentPlayer = player1Turn ? player1 : player2;
+        currentMarker = currentPlayer.getMarker();
+
+        success = board.markSquare(currentMarker, square)
+
+        if (success){
+            player1Turn = !player1Turn;
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    function whoseTurn(){
+        // Why doesn't this work??
+        // player1Turn ? player1 : player2
+        if (player1Turn) {
+            return player1;
+        } else {
+            return player2;
+        }
+    };
+
+    return {playRound, whoseTurn, newGame}
 })()
 
-function newPlayer(marker){
-    const marker = marker;
+function newPlayer(playerMarker){
+    const marker = playerMarker;
     const name = "";
 
     function setName(newName){
@@ -48,4 +83,4 @@ function newPlayer(marker){
     const getMarker = () => marker;
 
     return {setName, getName, getMarker}
-}
+};
