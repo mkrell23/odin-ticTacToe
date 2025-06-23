027a6367fd52fb250c2ffe04016e45940e0eac07
Matt <50344355+mkrell23@users.noreply.github.com>
2025-06-23 09:50:27 -0400

    Board object created

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,41 @@
 const p = document.getElementById("edit")
 p.innerText = "I'm here too";
 console.log("and here");
+
+
+
+
+
+const board = (function(){
+    const playBoard = {};
+
+    function newBoard(){
+        for (i = 1; i <= 9; i++){
+            playBoard[i] = "";
+        };
+
+        return playBoard;
+    }
+
+    function showBoard(){
+        return playBoard
+    }
+
+    function markSquare(marker, position){
+        if ( playBoard[position] === ""){
+            playBoard[position] = marker;
+        }
+        return playBoard
+    }
+
+    newBoard()
+
+
+    return {showBoard, markSquare, newBoard};
+})()
+
+const game = (function(){
+    function playRound(){
+
+    }
+})()
